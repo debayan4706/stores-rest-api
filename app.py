ede1b2a989dcf14787d9c8d38ffa770a6595744d
Debayan <debayan_chttrj@yahoo.co.in>
2022-07-26 11:58:52 +0530

    Fix misc errors

--- app.py
+++ app.py
@@ -4,18 +4,13 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
-from resources.store import Store, StoreList
-from db import db
+from resources.store import Store, StoreList   
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'debayan'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)  #=> Would create a new Endpoint - /auth. Would send username & password
 
@@ -26,6 +21,7 @@ api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':                  
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)             ##debug=True - this will give an html page of error details in case of any error
 
